7812796dda47f7ceabb366548b321098034297f3
Tetrahedran <paddy38@web.de>
2017-11-28 11:28:54 +0100

    InQueueLimit zum Stationtype hinzugefügt Setter und Getter hinzugefügt

--- src/model/StationType.java
+++ src/model/StationType.java
@@ -1,12 +1,19 @@
 package model;
 
 /**
- * defines several types of Stations
+ * defines several types of Stations with their InQueueLimit
  * @author Patrick Hanselmann, Sebastian Herzog, Jeffrey Manuel Rietzler, Nils Clauss
  * @version 2017-11-28
  */
 public enum StationType {
-    VORSPEISE(0), HAUPTSPEISE(0), NACHSPEISE(0), KASSE(0), DEFAULT(0), START(0), ENDE(0), EXTRA(0);
+    VORSPEISE(0), HAUPTSPEISE(0), NACHSPEISE(0), KASSE(0), DEFAULT(0), START(0), ENDE(0), ADDITIONAL(0);
+
+
+    private int inQueueLimit;
+
+    StationType(int inQueueLimit){
+        this.inQueueLimit = inQueueLimit;
+    }
 
     /**
      * parser for StationTypes
@@ -14,12 +21,6 @@ public enum StationType {
      * @return the StationType corresponding to the InputString,
      * if no Type fits DEFAULT is returned
      */
-    private int inQueueLimit;
-
-    StationType(int inQueueLimit){
-        this.inQueueLimit = inQueueLimit;
-    }
-
     public static StationType parseStationType(String str){
         for(StationType type: StationType.values()){
             if(str.toUpperCase().equals(type.toString())){
@@ -29,10 +30,18 @@ public enum StationType {
         return DEFAULT;
     }
 
+    /**
+     * setter for the InQueueLimit
+     * @param inQueueLimit the new inQueueLimit
+     */
     public void setInQueueLimit(int inQueueLimit) {
         this.inQueueLimit = inQueueLimit;
     }
 
+    /**
+     * getter for the inQueueLimit
+     * @return the inQueueLimit
+     */
     public int getInQueueLimit() {
         return inQueueLimit;
     }
