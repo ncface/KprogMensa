637f05d118f8128f9eab8e7161a509dfc3982d9c
Tetrahedran <paddy38@web.de>
2017-11-29 11:11:03 +0100

    ScenarioDialog beendet das Programm wenn es geschlossen wird

--- src/view/SelectionDialog.java
+++ src/view/SelectionDialog.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -100,7 +102,12 @@ public final class SelectionDialog extends JDialog implements Runnable{
         main.add(szenarioSelectionPanel);
         main.add(buttonPanel);
 
-        this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
 
         this.setModal(true);
         this.pack();
