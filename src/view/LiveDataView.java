83c836804efbe073d806c8f7515f049acaca8144
Tetrahedran <paddy38@web.de>
2017-12-04 16:07:44 +0100

    Interface LiveDataView hinzugefügt

---
+++ src/view/LiveDataView.java
@@ -0,0 +1,19 @@
+package view;
+
+import model.Station;
+
+/**
+ * An interface that represents a Graph that displays live data while the simulation works
+ * @author Hanselmann, Clauss, Rietzler, Herzog
+ * @version 4.12.17
+ */
+public interface LiveDataView {
+
+    /**
+     * A Method to add another point to the live data diagram
+     * @param station the station to which the data point shall be assigned
+     * @param x the x value of the data point
+     * @param y the y value of the data point
+     */
+    void addPointToDiagramm(Station station, int x, int y);
+}
