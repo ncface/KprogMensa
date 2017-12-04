9ac3d825e5212829c2fbdd35351d47f6d0064ea8
JeffreyRie <j.rietzler@gmx.de>
2017-12-04 19:23:00 +0100

    customer observer implementiert

--- src/io/DataCollection.java
+++ src/io/DataCollection.java
@@ -6,10 +6,7 @@ import view.GraphPlotterArray;
 import view.LiveDataView;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * A class to collect and save data from the simulation to files
